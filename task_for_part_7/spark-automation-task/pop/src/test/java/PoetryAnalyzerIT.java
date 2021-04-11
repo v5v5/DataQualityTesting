05c3ef3e157ebd87bb524b6e0108d50910215592
Vitalii Balitckii <vitalii_balitckii@epam.com>
2021-04-11 12:09:36 +0300

    added PoetryAnalyzerIT#testDataIsMissingAfterDataMigration

--- task_for_part_7/spark-automation-task/pop/src/test/java/PoetryAnalyzerIT.java
+++ task_for_part_7/spark-automation-task/pop/src/test/java/PoetryAnalyzerIT.java
@@ -1,18 +1,28 @@
+import static org.apache.commons.io.FileUtils.copyDirectory;
+import static org.apache.commons.io.FileUtils.deleteDirectory;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.epam.pop.AppConfig;
 import com.epam.pop.PoetryAnalyzer;
+import com.epam.pop.WordData;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.junit.jupiter.api.Assertions;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
@@ -57,51 +67,101 @@ public class PoetryAnalyzerIT {
     @Test
     public void testTheResultOfMostPopularWordsIsTheSame() throws IOException {
         // arrange
-//        var poetList = List.of("2pac", "gaga", "perry", "tool");
-        var poetList = Files.list(Path.of(path))
+//        var popStarsList = List.of("2pac", "gaga", "perry", "tool");
+        var popStarsList = Files.list(Path.of(path))
             .map(d->d.getFileName().toString())
             .collect(Collectors.toList());
-        System.out.println(poetList);
+        System.out.println(popStarsList);
 
-        for (var poet: poetList) {
-            System.out.println(poet);
-            // act
-            var expectedWords = analyzer.mostPopularWords(poet, 5);
+        // act
+        var assertionList = new ArrayList<Executable>();
+        popStarsList.forEach(popStar -> {
+            System.out.println(popStar);
+            var expectedWords = analyzer.mostPopularWords(popStar, 5);
             System.out.println(expectedWords);
-            var actualWords = analyzer.mostPopularWords(poet, 5);
-            // assert
-            Assertions.assertEquals(expectedWords, actualWords);
-        }
+            var actualWords = analyzer.mostPopularWords(popStar, 5);
+
+            assertionList.add(() -> assertEquals(expectedWords, actualWords));
+        });
+
+        // assert
+        assertAll(assertionList);
     }
 
     @Test
     public void testTheUnexpectedDataDoesNotTotallyBreakTheService() {
         // arrange
-        var unexpectedPoet = RandomStringUtils.randomAlphanumeric(10);
+        var unexpectedPopStar = RandomStringUtils.randomAlphanumeric(10);
 
         // act
         try {
-            var expectedWords = analyzer.mostPopularWords(unexpectedPoet, 5);
+            var expectedWords = analyzer.mostPopularWords(unexpectedPopStar, 5);
         } catch (Exception ex) {
             System.out.println("The unexpected data cause Exception");
             ex.printStackTrace();
         }
 
         // assert
-        var poet = "perry";
-        var expectedWords = analyzer.mostPopularWords(poet, 5);
-        var actualWords = analyzer.mostPopularWords(poet, 5);
-        Assertions.assertEquals(expectedWords, actualWords);
+        var popStar = "perry";
+        var expectedWords = analyzer.mostPopularWords(popStar, 5);
+        var actualWords = analyzer.mostPopularWords(popStar, 5);
+        assertEquals(expectedWords, actualWords);
         System.out.println("The service is still working");
     }
 
     @Test
     public void testEncodingIncompatibilitiesCauseOutputResultIsNotCorrect(){
-        var poet = "perry";
-        byte[] bytes = poet.getBytes(StandardCharsets.UTF_8);
-        poet = new String(bytes, StandardCharsets.UTF_16);
+        var popStar = "perry";
+        byte[] bytes = popStar.getBytes(StandardCharsets.UTF_8);
+        popStar = new String(bytes, StandardCharsets.UTF_16);
 
-        String finalPoet = poet;
-        Assertions.assertThrows(Exception.class, () -> analyzer.mostPopularWords(finalPoet, 5));
+        String finalPopStar = popStar;
+        assertThrows(Exception.class, () -> analyzer.mostPopularWords(finalPopStar, 5));
+    }
+
+    @Autowired
+    private WordData wordData;
+
+    @Test
+    public void testDataIsMissingAfterDataMigration() throws IOException {
+        // arrange
+        System.out.println("Migration with losses is started...");
+        var migrationPath = ".\\migration\\";
+        File srcDirectory = new File(path);
+        File dstDirectory = new File(migrationPath);
+
+        deleteDirectory(dstDirectory);
+        copyDirectory(srcDirectory, dstDirectory);
+
+        for (var popstarDir: dstDirectory.listFiles()) {
+            System.out.println(
+                Arrays.stream(popstarDir.listFiles())
+                    .map(f -> f.getName())
+                    .collect(Collectors.toList()));
+            var popstarFiles = popstarDir.listFiles();
+            var randomIndex = new Random()
+                .ints(0, popstarFiles.length)
+                .findFirst()
+                .getAsInt();
+            FileUtils.forceDelete(popstarFiles[randomIndex]);
+            System.out.println(
+                Arrays.stream(popstarDir.listFiles())
+                    .map(f -> f.getName())
+                    .collect(Collectors.toList()));
+        }
+        System.out.println("Migration with losses is done.");
+
+        // act
+        Dataset<Row> dataBeforeMigration = wordData.create(path + "*");
+        Dataset<Row> dataAfterMigration = wordData.create(migrationPath + "*");
+
+        // assert
+        assertAll(
+            () -> assertEquals(dataBeforeMigration.count(), dataAfterMigration.count()),
+            () -> {
+                deleteDirectory(dstDirectory);
+                System.out.println(String.format("After test: directory '%s' is deleted", dstDirectory));
+            }
+        );
     }
 }
