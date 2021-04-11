1e4b00d00379ec479537d085a4ba1df8a1b7d22d
Vitalii Balitckii <vitalii_balitckii@epam.com>
2021-04-11 10:01:45 +0300

    added PoetryAnalyzerIT#testTheResultOfMostPopularWordsIsTheSame to task 7

--- task_for_part_7/spark-automation-task/pop/src/main/java/com/epam/pop/AppConfig.java
+++ task_for_part_7/spark-automation-task/pop/src/main/java/com/epam/pop/AppConfig.java
@@ -17,13 +17,8 @@ public class AppConfig {
     @Bean
     public JavaSparkContext cxt() {
 
-        var properties = System.getProperties();
-        properties.put("spark.sql.warehouse.dir", "file:///C:/MyData/spark-automation-task/pop/spark-warehouse");
-        properties.put("HADOOP_HOME", "C:/hadoop");
-        System.setProperties(properties);
-
         SparkConf conf = new SparkConf().setAppName("pop").setMaster("local[4]");
-        return  new JavaSparkContext(conf);
+        return new JavaSparkContext(conf);
     }
 
     @Bean
--- task_for_part_7/spark-automation-task/pop/src/test/java/PoetryAnalyzerIT.java
+++ task_for_part_7/spark-automation-task/pop/src/test/java/PoetryAnalyzerIT.java
@@ -1,8 +1,18 @@
 import com.epam.pop.AppConfig;
 import com.epam.pop.PoetryAnalyzer;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -37,5 +47,28 @@ public class PoetryAnalyzerIT {
     public void testGaga() {
         var words = analyzer.mostPopularWords("2pac", 3);
         System.out.println(words.toString());
+    }
+
+    @Value("${rootPath}")
+    private String path;
+
+    @Test
+    public void testTheResultOfMostPopularWordsIsTheSame() throws IOException {
+        // arrange
+//        var poetryList = List.of("2pac", "gaga", "perry", "tool");
+        var poetryList = Files.list(Path.of(path))
+            .map(d->d.getFileName().toString())
+            .collect(Collectors.toList());
+        System.out.println(poetryList);
+
+        for (var poetry: poetryList) {
+            System.out.println(poetry);
+            // act
+            var expectedWords = analyzer.mostPopularWords(poetry, 5);
+            System.out.println(expectedWords);
+            var actualWords = analyzer.mostPopularWords(poetry, 5);
+            // assert
+            Assertions.assertEquals(expectedWords, actualWords);
+        }
     }
 }
